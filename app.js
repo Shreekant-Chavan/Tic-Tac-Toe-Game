adf0b6d07fc584930c608ef8fca41ea542e9f0ed
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-05-25 23:56:38 +0530

    Some JS Logics added

--- app.js
+++ app.js
@@ -1,2 +1,88 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("p")
+
+
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("Button was clicked!");
+        if (turnO) {
+            box.innerText = "X";
+            turnO = false;
+        } else {
+            box.innerText = "O";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    })
+})
+
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation! Winner is ${winner}`
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos0val = boxes[pattern[0]].innerText;
+        let pos1val = boxes[pattern[1]].innerText;
+        let pos2val = boxes[pattern[2]].innerText;
+
+        if (pos0val != "" && pos1val != "" && pos2val != "") {
+            if (pos0val === pos1val && pos1val === pos2val) {
+                console.log("Winner!",pos0val);
+                showWinner(pos0val);
+            }
+        }
+        // console.log(pattern[0], pattern[1], pattern[2]);
+        // console.log(
+        //     boxes[pattern[0]].innerText,
+        //     boxes[pattern[1]].innerText,
+        //     boxes[pattern[2]].innerText
+        // );
+
+    }
+}
+
+
+newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
